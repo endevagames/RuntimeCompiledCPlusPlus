413e836772358997bda0b0adb6471b62c7b31cf9
Samuel Mungy <samuel.mungy@outlook.com>
2022-05-12 11:46:25 +0300

    Editor configurations

--- premake5.lua
+++ premake5.lua
@@ -31,9 +31,22 @@ project "RuntimeCompiler"
 		runtime "Debug"
 		symbols "on"
 
+	filter "configurations:DebugEditor"
+		runtime "Debug"
+		symbols "on"
+		
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+
+	filter "configurations:ReleaseEditor"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
 
 project "RuntimeObjectSystem"
 	kind "StaticLib"
@@ -67,8 +80,20 @@ project "RuntimeObjectSystem"
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
-	
+
+	filter "configurations:DebugEditor"
+		runtime "Debug"
+		symbols "on"
+		
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-	
+
+	filter "configurations:ReleaseEditor"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
